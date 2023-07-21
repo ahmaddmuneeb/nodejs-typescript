8b73626157c9501075a9850db9b35847d99c9aa7
Muneeb Ahmad <muneeb.ahmad@purelogics.net>
2023-07-21 17:04:00 +0500

    project initial setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+require('dotenv').config();
+import { startServer } from './config/server';
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+// server config
+startServer(port);
